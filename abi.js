62adb5d5ea23f3906f184d7a6a60b5b31b032160
jessiewangkgi-max <jessiewangkgi@gmail.com>
2026-01-01 16:17:52 +0800

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,10 +1,23 @@
-// InternalGuessGame 最小可用 ABI（只讀）
+// InternalGuessGame 最小可用 ABI（讀題目）
 const GUESS_ABI = [
   {
     "type": "function",
     "name": "questionsCount",
     "inputs": [],
     "outputs": [{ "type": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "getQuestion",
+    "inputs": [{ "name": "questionId", "type": "uint256" }],
+    "outputs": [
+      { "name": "text", "type": "string" },
+      { "name": "options", "type": "string[]" },
+      { "name": "status", "type": "uint8" },
+      { "name": "winningOption", "type": "uint256" },
+      { "name": "totalPool", "type": "uint256" }
+    ],
     "stateMutability": "view"
   }
 ];
