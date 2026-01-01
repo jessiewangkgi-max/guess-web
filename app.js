8c638fa6d4f9d80206f9ca2b40991d3c6b915b40
jessiewangkgi-max <jessiewangkgi@gmail.com>
2026-01-01 16:16:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const GUESS_ADDRESS = "0x483aee89c55737eceaab61c4ffe0e74b0f88e4a8";
+
 async function connect() {
   try {
     if (!window.ethereum) {
@@ -8,19 +10,29 @@ async function connect() {
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
     const addr = accounts[0];
 
-    // 讀 chainId（hex）
     const chainIdHex = await window.ethereum.request({ method: "eth_chainId" });
 
-    // ethers provider
     const provider = new ethers.BrowserProvider(window.ethereum);
-    const network = await provider.getNetwork(); // 有些錢包會回傳數字 chainId
+    const network = await provider.getNetwork();
 
     document.getElementById("msg").textContent = "✅ 已連線錢包：" + addr;
     document.getElementById("chain").textContent =
       `目前 chainId：${chainIdHex}（ethers 看到的是 ${network.chainId.toString()}）`;
 
+    if (chainIdHex !== "0xaa36a7") {
+      document.getElementById("qc").textContent = "❌ 請切到 Sepolia 再讀合約";
+      return;
+    }
+
+    const guess = new ethers.Contract(GUESS_ADDRESS, GUESS_ABI, provider);
+    const count = await guess.questionsCount();
+
+    document.getElementById("qc").textContent =
+      `✅ 合約題目數量 questionsCount = ${count.toString()}`;
+
   } catch (err) {
-    document.getElementById("msg").textContent = "❌ 連線失敗：" + err.message;
+    document.getElementById("qc").textContent = "❌ 讀合約失敗：" + (err.message || err);
+    console.error(err);
   }
 }
 
