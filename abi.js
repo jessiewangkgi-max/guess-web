0ea17733c0f37228016281dfe44860e6b35b1f73
jessiewangkgi-max <jessiewangkgi@gmail.com>
2026-01-01 17:00:14 +0800

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,113 +1,44 @@
 const GUESS_ABI = [
-  // --- Ownable ---
+  { "type":"function","name":"betToken","inputs":[],"outputs":[{"type":"address"}],"stateMutability":"view" },
+  { "type":"function","name":"questionsCount","inputs":[],"outputs":[{"type":"uint256"}],"stateMutability":"view" },
   {
-    "type": "function",
-    "name": "owner",
-    "inputs": [],
-    "outputs": [{ "type": "address" }],
-    "stateMutability": "view"
+    "type":"function","name":"getQuestion",
+    "inputs":[{"name":"questionId","type":"uint256"}],
+    "outputs":[
+      {"name":"text","type":"string"},
+      {"name":"options","type":"string[]"},
+      {"name":"status","type":"uint8"},
+      {"name":"winningOption","type":"uint256"},
+      {"name":"totalPool","type":"uint256"}
+    ],
+    "stateMutability":"view"
+  },
+  {
+    "type":"function","name":"totalStakedPerOption",
+    "inputs":[{"name":"questionId","type":"uint256"},{"name":"optionId","type":"uint256"}],
+    "outputs":[{"type":"uint256"}],
+    "stateMutability":"view"
+  },
+  {
+    "type":"function","name":"userStake",
+    "inputs":[{"name":"questionId","type":"uint256"},{"name":"user","type":"address"},{"name":"optionId","type":"uint256"}],
+    "outputs":[{"type":"uint256"}],
+    "stateMutability":"view"
+  },
+  {
+    "type":"function","name":"claimed",
+    "inputs":[{"name":"questionId","type":"uint256"},{"name":"user","type":"address"}],
+    "outputs":[{"type":"bool"}],
+    "stateMutability":"view"
   },
 
-  // --- token getter ---
-  {
-    "type": "function",
-    "name": "betToken",
-    "inputs": [],
-    "outputs": [{ "type": "address" }],
-    "stateMutability": "view"
-  },
+  // ✅ 出題（你說 Remix 已經能用）
+  { "type":"function","name":"createQuestion","inputs":[{"name":"text","type":"string"},{"name":"options","type":"string[]"}],"outputs":[{"type":"uint256"}],"stateMutability":"nonpayable" },
 
-  // --- views ---
-  {
-    "type": "function",
-    "name": "questionsCount",
-    "inputs": [],
-    "outputs": [{ "type": "uint256" }],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "getQuestion",
-    "inputs": [{ "name": "questionId", "type": "uint256" }],
-    "outputs": [
-      { "name": "text", "type": "string" },
-      { "name": "options", "type": "string[]" },
-      { "name": "status", "type": "uint8" },           // 0 Open, 1 Resolved
-      { "name": "winningOption", "type": "uint256" },
-      { "name": "totalPool", "type": "uint256" }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "totalStakedPerOption",
-    "inputs": [
-      { "name": "questionId", "type": "uint256" },
-      { "name": "optionId", "type": "uint256" }
-    ],
-    "outputs": [{ "type": "uint256" }],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "userStake",
-    "inputs": [
-      { "name": "questionId", "type": "uint256" },
-      { "name": "user", "type": "address" },
-      { "name": "optionId", "type": "uint256" }
-    ],
-    "outputs": [{ "type": "uint256" }],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "claimed",
-    "inputs": [
-      { "name": "questionId", "type": "uint256" },
-      { "name": "user", "type": "address" }
-    ],
-    "outputs": [{ "type": "bool" }],
-    "stateMutability": "view"
-  },
-
-  // --- tx: admin ---
-  {
-    "type": "function",
-    "name": "createQuestion",
-    "inputs": [
-      { "name": "text", "type": "string" },
-      { "name": "options", "type": "string[]" }
-    ],
-    "outputs": [{ "type": "uint256" }],
-    "stateMutability": "nonpayable"
-  },
-
-  // --- tx: user ---
-  {
-    "type": "function",
-    "name": "bet",
-    "inputs": [
-      { "name": "questionId", "type": "uint256" },
-      { "name": "optionId", "type": "uint256" },
-      { "name": "amount", "type": "uint256" }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "claim",
-    "inputs": [{ "name": "questionId", "type": "uint256" }],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "refund",
-    "inputs": [{ "name": "questionId", "type": "uint256" }],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  }
+  // bet/claim/refund
+  { "type":"function","name":"bet","inputs":[{"name":"questionId","type":"uint256"},{"name":"optionId","type":"uint256"},{"name":"amount","type":"uint256"}],"outputs":[],"stateMutability":"nonpayable" },
+  { "type":"function","name":"claim","inputs":[{"name":"questionId","type":"uint256"}],"outputs":[],"stateMutability":"nonpayable" },
+  { "type":"function","name":"refund","inputs":[{"name":"questionId","type":"uint256"}],"outputs":[],"stateMutability":"nonpayable" }
 ];
 
 const ERC20_ABI = [
