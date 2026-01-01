001345e383b1f32d9477d61b36655d399b58541f
jessiewangkgi-max <jessiewangkgi@gmail.com>
2026-01-01 16:15:33 +0800

    Create abi.js

---
+++ abi.js
@@ -0,0 +1,10 @@
+// InternalGuessGame 最小可用 ABI（只讀）
+const GUESS_ABI = [
+  {
+    "type": "function",
+    "name": "questionsCount",
+    "inputs": [],
+    "outputs": [{ "type": "uint256" }],
+    "stateMutability": "view"
+  }
+];
